5aa39dde383834f3f5aec6451ff64b81c7247b72
Dewz <dewzzpro@gmail.com>
2020-12-22 02:19:13 +0700

    bug: fix typo makefile

--- go-build.sh
+++ go-build.sh
@@ -11,7 +11,7 @@ for platform in "${platforms[@]}"
 do
     platform_split=(${platform//\// })
     GOOS=${platform_split[0]}
-    GOARCH=${platform_split[1split]}
+    GOARCH=${platform_split[1]}
     output_name=$package_name'-'$GOOS'-'$GOARCH
     if [ $GOOS = "windows" ]; then
         output_name+='.exe'
