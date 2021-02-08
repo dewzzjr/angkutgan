9996744c8906bcb202e0b91d7d7bf22ba6f9a64b
bdulkim <kim.gendos@gmail.com>
2021-02-08 22:43:41 +0700

    update buat transaksi di tab penjualan

--- frontend/assets/js/penjualan.js
+++ frontend/assets/js/penjualan.js
@@ -65,12 +65,24 @@ const Sales = {
 };
 
 $(document).ready(function () {
-  //header();
-  
+  header();
+
+  // Convert number to currency
   function currency($param){
-    return 'Rp'+($param.toLocaleString());
+    var number = new Intl.NumberFormat('id-ID', { 
+        style: 'currency', 
+        currency: 'IDR' 
+    }).format($param);
+    return number;
+  }
+
+  // Convert currency to integer
+  function double(currency){  
+    var temp = currency.replace(/[^0-9,-]+/g,""); 
+    return parseInt(temp); 
   }
   
+  // Convert number with adding comma per 3 digits
   $('input.number').keyup(function(event) {
     // skip for arrow keys
     if(event.which >= 37 && event.which <= 40) return;
@@ -79,7 +91,7 @@ $(document).ready(function () {
     $(this).val(function(index, value) {
       return value
       .replace(/\D/g, "")
-      .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ".")
       ;
     });
   });
@@ -130,40 +142,81 @@ $(document).ready(function () {
       $('#priceBarang').val(currency(b.price.sell));
       $('#unit span').text(b.unit);
       $("#countBarang").on("change paste keyup", function() {
-        $('#totalBarang').attr('value', currency(($(this).val().replace(",", "")*b.price.sell))); 
-      });
-
-      $('#tambah').click(function(){
-        $("#tableBarang tbody").prepend(
-          `<tr>
-            <th scope="row">${b.code}</th>
-            <td>${$("#countBarang").val()} ${b.unit}</td>
-            <td>${currency(b.price.sell)}</td>
-            <th>${$('#totalBarang').val()}</th>
-            <td>
-                <button type="button" class="btn btn-warning">Ubah</button>
-                <button type="button" class="btn btn-danger">Hapus</button>
-            </td>
-          </tr>`
-        );
-
-        $('#setCode').val('');
-        $('#nameEdit').val('');
-        $('#priceBarang').val('');
-        $('#unit span').val('');
-        $("#countBarang").val('');
-        $('#totalBarang').attr('value', '');
+        $('#totalBarang').attr('value', currency((double($(this).val())*b.price.sell))); 
       });
     });
   });
 
-  /* $("#deliveryFee").on("change paste keyup", function() {
-    $('#ongkir').text('Ongkos Kirim = Rp' + $(this).val());
+  // Tambah Barang to list Item
+	var rowIdx = 0;
+  $('#tambah').on('click', function () { 
+		$('#listItem').append(
+	    `<tr id="item${++rowIdx}">
+				<th scope="row">${$('#addCodeBarang').val()}</th>
+				<td id="listJumlah">${$('#countBarang').val()}</td>
+	      <td id="listHarga">${$('#priceBarang').val()}</td>
+	      <th id="listTotal">${$('#totalBarang').val()}</th>
+	      <td align="right">
+	       	<button type="button" class="btn btn-warning edit">Ubah</button>
+	       	<button type="button" class="btn btn-danger remove">Hapus</button>
+	      </td>
+			</tr>`);
+    $('#ringkasanItem').append(
+      `<li class="list-group-item text-right" id="item${++rowIdx-1}">
+      ${$('#nameEdit').val()} &times; ${$('#countBarang').val()} ${$('#unit span').text()} = <span>${$('#totalBarang').val()}</span>
+      </li>`);
+    $('#addCodeBarang').val('').focus();
+    $('#nameEdit').val('');
+		$('#countBarang').val('');
+    $('#unit span').text('');
+	  $('#priceBarang').val('');
+	  $('#totalBarang').attr('value', '');
+	}); 
+
+  // Delete Barang from list Item
+	$('#listItem').on('click', '.remove', function () { 
+		var tr = $(this).closest('tr'); 
+    var id = tr.attr('id');
+    $('[id="'+id+'"]').remove();
+		rowIdx--; 
+	});
+
+  // Edit Barang from list Item
+  $("#listItem").on("click", ".edit", function() {
+    var row = $(this).closest('tr');
+    var id = row.attr('id');
+    if ($('.edit').text() == 'Update'){
+      row.find("#listJumlah").text($('#editListJumlah').val());
+      row.find("#listHarga").text($('#editListHarga').val());
+      row.find("#listTotal").text($('#editListTotal').val());
+      $('.edit').text('Edit');
+      $('[id="'+id+'"] span').text($('#editListTotal').val());
+    } else {
+      row.find("#listJumlah").html(`<input class="form-control number" id="editListJumlah">`);
+      row.find("#listHarga").html(`<input disabled class="form-control" id="editListHarga" value="${$('#listHarga').text()}">`);
+      row.find("#listTotal").html(`<input disabled class="form-control" id="editListTotal" value="${$('#listTotal').text()}">`);
+      $('#editListJumlah').focus();
+      $('.edit').text('Update');
+    }
+
+    $("#editListJumlah").on("change paste keyup", function() {
+      var tot = double($(this).val())*double($("#editListHarga").val());
+      if (isNaN(tot)) {
+        $('#editListTotal').attr('value', currency(0));
+      } else {
+        $('#editListTotal').attr('value', currency(tot));
+      } 
+    });
   });
 
-  if ($("input:empty").length == 0){
-    $('#total').text('Total Tagihan = Rp' + $('#ongkir').val());
-  }*/ 
-
+  // Update ringkasan ongkir
+  $("#deliveryFee").on("change paste keyup", function() {
+    var ongkir = double($(this).val());
+    if (isNaN(ongkir)) {
+      $('#ongkir').text('Ongkos Kirim = ' + currency(0));
+    } else {
+      $('#ongkir').text('Ongkos Kirim = ' + currency(ongkir));
+    }
+  });
 
 });
